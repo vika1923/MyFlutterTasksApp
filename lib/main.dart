4637b80d490c6001f4d34bb4ffd578661b3994aa
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-07-22 22:09:09 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:my_app1/task.dart';
+import 'package:my_app1/add_new_task.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AllTasks()));
+}
 
-class MyApp extends StatelessWidget {
-  final tasks = ['task1', 'task2', 'task3'];
+class AllTasks extends StatefulWidget {
+  const AllTasks({super.key});
 
   @override
+  State<AllTasks> createState() => _AllTasksState();
+}
+
+class _AllTasksState extends State<AllTasks> {
+  List<String> tasks = ["task1", "task2"];
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("test"),
-        ),
-        body: ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (context, index){
-            return Task();
-          },
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, builder) {
+                  return Task();
+                }),
+          )),
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AddNewTask(),
+              );
+            },
+            icon: Icon(Icons.add),
+          )
+        ],
       ),
     );
   }
