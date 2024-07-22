db01cac8f9aaedb1e7e5b2671b9a6ae72310bc94
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-07-22 22:10:02 +0500

    Create hours.dart

---
+++ lib/hours.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyHours extends StatelessWidget {
+
+  final int hours;
+  MyHours({required this.hours});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(0.5),
+      child: Container(
+        child: Center(
+          child: Text(
+            hours.toString(),
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
