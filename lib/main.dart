6cb3b7513d4224a60c3deb05214941b8d95b59aa
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-07-24 20:32:53 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_app1/task.dart';
 import 'package:my_app1/add_new_task.dart';
 
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AllTasks()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('tasksBox');
+
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: AllTasks()));
 }
 
 class AllTasks extends StatefulWidget {
@@ -14,28 +20,73 @@ class AllTasks extends StatefulWidget {
 }
 
 class _AllTasksState extends State<AllTasks> {
-  List<String> tasks = ["task1", "task2"];
+  List<List<dynamic>> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadTasks();
+  }
+
+  void loadTasks() {
+  try {
+    var box = Hive.box('tasksBox');
+    setState(() {
+      tasks = box.values.toList().cast<List<dynamic>>();
+    });
+  } catch (e) {
+    print("Error loading tasks: $e");
+  }
+}
+
+void deleteTask(int index){
+  var box = Hive.box('tasksBox');
+  setState(() {
+    tasks.removeAt(index);
+    box.deleteAt(index);
+  });
+}
+
+void deleteBox() async {
+    var box = await Hive.openBox('tasksBox');
+    await box.deleteFromDisk();
+    setState(() {
+      tasks = [];
+    });
+    print("Box deleted");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-              child: Container(
             child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, builder) {
-                  return Task();
-                }),
-          )),
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return Task(
+                  taskName: tasks[index][0],
+                  hours: tasks[index][1],
+                  minutes: tasks[index][2],
+                  priority: tasks[index][3],
+                  deleteTask: () => deleteTask(index),
+                  key: ValueKey(tasks[index]),
+                );
+              },
+            ),
+          ),
           IconButton(
             onPressed: () {
+              // deleteBox();
               showDialog(
                 context: context,
                 builder: (context) => AddNewTask(),
-              );
+              ).then((_) {
+                loadTasks();
+              });
             },
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
           )
         ],
       ),
