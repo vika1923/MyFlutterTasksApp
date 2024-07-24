54595603acb941559830811cb72a0fe5d1c354e0
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-07-24 20:35:39 +0500

    Add files via upload

--- lib/task.dart
+++ lib/task.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:my_app1/timer.dart';
 
-class Task extends StatelessWidget{
-  
-  // final int hours ;
-  // final int minutes ;
-  // final String taskName ;
+class Task extends StatelessWidget {
+  final int hours;
+  final int minutes;
+  final String taskName;
+  final String priority;
+  final VoidCallback deleteTask;
 
-  // Task({required this.hours, required this.minutes, required this.taskName});
+  Task({
+    required this.hours,
+    required this.minutes,
+    required this.taskName,
+    required this.priority,
+    required this.deleteTask,
+    Key? key, // Accept a key as a parameter
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return ListTile(
-      minVerticalPadding: 20,
-      title: const Text("Thing to do", style: TextStyle(fontWeight: FontWeight.bold)),
-      subtitle: ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-        child: Container(
-          height: 100,
-          color: Colors.lightBlue[100],
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.blue[100],
         ),
-       )
+        child: ListTile(
+          minVerticalPadding: 20,
+          title: Text(taskName, style: const TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: Text("Duration: $hours:$minutes, Priority: $priority"),
+          tileColor: Colors.transparent,
+          leading: IconButton(
+            icon: const Icon(Icons.delete_outlined),
+            onPressed: deleteTask,
+          ),
+          trailing: Container(
+            width: 80,
+            height: 70,
+            child: MyTimer(timeLeft: hours * 60 + minutes, taskName: taskName),
+          ),
+        ),
+      ),
     );
   }
 }
